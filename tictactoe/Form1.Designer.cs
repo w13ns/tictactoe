6317f16901e379313fbc3936e5b35f7c0c6ba597
w13ns <104914961+w13ns@users.noreply.github.com>
2022-05-04 18:39:49 +0400

    Add files via upload

---
+++ tictactoe/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l7 = new System.Windows.Forms.Label();
+            this.l8 = new System.Windows.Forms.Label();
+            this.l9 = new System.Windows.Forms.Label();
+            this.l6 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.whonow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // l7
+            // 
+            this.l7.AutoSize = true;
+            this.l7.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l7.Location = new System.Drawing.Point(13, 9);
+            this.l7.Name = "l7";
+            this.l7.Size = new System.Drawing.Size(19, 21);
+            this.l7.TabIndex = 0;
+            this.l7.Text = "*";
+            this.l7.Click += new System.EventHandler(this.l7_Click);
+            this.l7.MouseLeave += new System.EventHandler(this.l7_MouseLeave);
+            this.l7.MouseHover += new System.EventHandler(this.l7_MouseHover);
+            // 
+            // l8
+            // 
+            this.l8.AutoSize = true;
+            this.l8.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l8.Location = new System.Drawing.Point(42, 9);
+            this.l8.Name = "l8";
+            this.l8.Size = new System.Drawing.Size(19, 21);
+            this.l8.TabIndex = 0;
+            this.l8.Text = "*";
+            this.l8.Click += new System.EventHandler(this.l8_Click);
+            this.l8.MouseLeave += new System.EventHandler(this.l8_MouseLeave);
+            this.l8.MouseHover += new System.EventHandler(this.l8_MouseHover);
+            // 
+            // l9
+            // 
+            this.l9.AutoSize = true;
+            this.l9.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l9.Location = new System.Drawing.Point(68, 9);
+            this.l9.Name = "l9";
+            this.l9.Size = new System.Drawing.Size(19, 21);
+            this.l9.TabIndex = 0;
+            this.l9.Text = "*";
+            this.l9.Click += new System.EventHandler(this.l9_Click);
+            this.l9.MouseLeave += new System.EventHandler(this.l9_MouseLeave);
+            this.l9.MouseHover += new System.EventHandler(this.l9_MouseHover);
+            // 
+            // l6
+            // 
+            this.l6.AutoSize = true;
+            this.l6.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l6.Location = new System.Drawing.Point(68, 35);
+            this.l6.Name = "l6";
+            this.l6.Size = new System.Drawing.Size(19, 21);
+            this.l6.TabIndex = 0;
+            this.l6.Text = "*";
+            this.l6.Click += new System.EventHandler(this.l6_Click);
+            this.l6.MouseLeave += new System.EventHandler(this.l6_MouseLeave);
+            this.l6.MouseHover += new System.EventHandler(this.l6_MouseHover);
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l3.Location = new System.Drawing.Point(68, 63);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(19, 21);
+            this.l3.TabIndex = 0;
+            this.l3.Text = "*";
+            this.l3.Click += new System.EventHandler(this.l3_Click);
+            this.l3.MouseLeave += new System.EventHandler(this.l3_MouseLeave);
+            this.l3.MouseHover += new System.EventHandler(this.l3_MouseHover);
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l2.Location = new System.Drawing.Point(42, 63);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(19, 21);
+            this.l2.TabIndex = 0;
+            this.l2.Text = "*";
+            this.l2.Click += new System.EventHandler(this.l2_Click);
+            this.l2.MouseLeave += new System.EventHandler(this.l2_MouseLeave);
+            this.l2.MouseHover += new System.EventHandler(this.l2_MouseHover);
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l1.Location = new System.Drawing.Point(13, 63);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(19, 21);
+            this.l1.TabIndex = 0;
+            this.l1.Text = "*";
+            this.l1.Click += new System.EventHandler(this.l1_Click);
+            this.l1.MouseLeave += new System.EventHandler(this.l1_MouseLeave);
+            this.l1.MouseHover += new System.EventHandler(this.l1_MouseHover);
+            // 
+            // l4
+            // 
+            this.l4.AutoSize = true;
+            this.l4.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l4.Location = new System.Drawing.Point(13, 35);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(19, 21);
+            this.l4.TabIndex = 0;
+            this.l4.Text = "*";
+            this.l4.Click += new System.EventHandler(this.l4_Click);
+            this.l4.MouseLeave += new System.EventHandler(this.l4_MouseLeave);
+            this.l4.MouseHover += new System.EventHandler(this.l4_MouseHover);
+            // 
+            // l5
+            // 
+            this.l5.AutoSize = true;
+            this.l5.Font = new System.Drawing.Font("Franklin Gothic Heavy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l5.Location = new System.Drawing.Point(42, 35);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(19, 21);
+            this.l5.TabIndex = 0;
+            this.l5.Text = "*";
+            this.l5.Click += new System.EventHandler(this.l5_Click);
+            this.l5.MouseLeave += new System.EventHandler(this.l5_MouseLeave);
+            this.l5.MouseHover += new System.EventHandler(this.l5_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(163, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "now:";
+            // 
+            // whonow
+            // 
+            this.whonow.AutoSize = true;
+            this.whonow.Location = new System.Drawing.Point(145, 142);
+            this.whonow.Name = "whonow";
+            this.whonow.Size = new System.Drawing.Size(12, 13);
+            this.whonow.TabIndex = 3;
+            this.whonow.Text = "x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(102, 87);
+            this.Controls.Add(this.whonow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l6);
+            this.Controls.Add(this.l9);
+            this.Controls.Add(this.l8);
+            this.Controls.Add(this.l7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "tic-tac-toe";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l7;
+        private System.Windows.Forms.Label l8;
+        private System.Windows.Forms.Label l9;
+        private System.Windows.Forms.Label l6;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label whonow;
+    }
+}
+
---
+++ tictactoe/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool Check(string all)
+        {
+            if (all.Contains("xxx") || l1.Text + l4.Text + l7.Text == "xxx" || l2.Text + l5.Text + l8.Text == "xxx" || l3.Text + l6.Text + l9.Text == "xxx" || l1.Text + l5.Text + l9.Text == "xxx" || l3.Text + l5.Text + l7.Text == "xxx")
+            {
+                MessageBox.Show("x won in the last game!");
+                restart();
+                return true;
+            }
+            if (all.Contains("ooo") || l1.Text + l4.Text + l7.Text == "ooo" || l2.Text + l5.Text + l8.Text == "ooo" || l3.Text + l6.Text + l9.Text == "ooo" || l1.Text + l5.Text + l9.Text == "ooo" || l3.Text + l5.Text + l7.Text == "ooo")
+            {
+                MessageBox.Show("o won in the last game!");
+                restart();
+                return true;
+            }
+            return false;
+        }
+        void iffull(string all, bool a)
+        {
+            if (!all.Contains('*') & a == false)
+            {
+                MessageBox.Show("There was a draw in the last game");
+                restart();
+            }
+        }
+        void isWLN()
+        {
+            while (true)
+            {
+                Thread.Sleep(300);
+                string all = $"{l1.Text + l2.Text + l3.Text}-{l4.Text + l5.Text + l6.Text}-{l7.Text + l8.Text + l9.Text}";
+                if (all == "--")
+                    Environment.Exit(0);
+                iffull(all, Check(all));
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(isWLN);
+            thread.Start();
+        }
+        void whonowChange()
+        {
+            switch (whonow.Text)
+            {
+                case "x":
+                    whonow.Text = "o";
+                    break;
+                case "o":
+                    whonow.Text = "x";
+                    break;
+            }
+        }
+        void restart()
+        {
+            l9.Text = "*"; 
+            l8.Text = "*"; 
+            l7.Text = "*";
+            l6.Text = "*";
+            l5.Text = "*";
+            l4.Text = "*";
+            l3.Text = "*";
+            l2.Text = "*";
+            l1.Text = "*";
+            whonow.Text = "x";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            restart();
+        }
+        private void l1_MouseHover(object sender, EventArgs e)
+        {
+            if(l1.Text == "*")
+                l1.ForeColor = Color.Green;
+            else
+                l1.ForeColor = Color.Red;
+        }
+
+        private void l1_MouseLeave(object sender, EventArgs e)
+        {
+            l1.ForeColor = Color.Empty;
+        }
+        private void l2_MouseHover(object sender, EventArgs e)
+        {
+            if (l2.Text == "*")
+                l2.ForeColor = Color.Green;
+            else
+                l2.ForeColor = Color.Red;
+        }
+
+        private void l2_MouseLeave(object sender, EventArgs e)
+        {
+            l2.ForeColor = Color.Empty;
+        }
+        private void l3_MouseHover(object sender, EventArgs e)
+        {
+            if (l3.Text == "*")
+                l3.ForeColor = Color.Green;
+            else
+                l3.ForeColor = Color.Red;
+        }
+
+        private void l3_MouseLeave(object sender, EventArgs e)
+        {
+            l3.ForeColor = Color.Empty;
+        }
+        private void l4_MouseHover(object sender, EventArgs e)
+        {
+            if (l4.Text == "*")
+                l4.ForeColor = Color.Green;
+            else
+                l4.ForeColor = Color.Red;
+        }
+
+        private void l4_MouseLeave(object sender, EventArgs e)
+        {
+            l4.ForeColor = Color.Empty;
+        }
+        private void l5_MouseHover(object sender, EventArgs e)
+        {
+            if (l5.Text == "*")
+                l5.ForeColor = Color.Green;
+            else
+                l5.ForeColor = Color.Red;
+        }
+
+        private void l5_MouseLeave(object sender, EventArgs e)
+        {
+            l5.ForeColor = Color.Empty;
+        }
+        private void l6_MouseHover(object sender, EventArgs e)
+        {
+            if (l6.Text == "*")
+                l6.ForeColor = Color.Green;
+            else
+                l6.ForeColor = Color.Red;
+        }
+
+        private void l6_MouseLeave(object sender, EventArgs e)
+        {
+            l6.ForeColor = Color.Empty;
+        }
+        private void l7_MouseHover(object sender, EventArgs e)
+        {
+            if (l7.Text == "*")
+                l7.ForeColor = Color.Green;
+            else
+                l7.ForeColor = Color.Red;
+        }
+
+        private void l7_MouseLeave(object sender, EventArgs e)
+        {
+            l7.ForeColor = Color.Empty;
+        }
+        private void l8_MouseHover(object sender, EventArgs e)
+        {
+            if (l8.Text == "*")
+                l8.ForeColor = Color.Green;
+            else
+                l8.ForeColor = Color.Red;
+        }
+
+        private void l8_MouseLeave(object sender, EventArgs e)
+        {
+            l8.ForeColor = Color.Empty;
+        }
+        private void l9_MouseHover(object sender, EventArgs e)
+        {
+            if (l9.Text == "*")
+                l9.ForeColor = Color.Green;
+            else
+                l9.ForeColor = Color.Red;
+        }
+
+        private void l9_MouseLeave(object sender, EventArgs e)
+        {
+            l9.ForeColor = Color.Empty;
+        }
+        private void l1_Click(object sender, EventArgs e)
+        {
+            if (l1.Text == "*")
+            {
+                l1.Text = whonow.Text;
+                l1.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l2_Click(object sender, EventArgs e)
+        {
+            if (l2.Text == "*")
+            {
+                l2.Text = whonow.Text;
+                l2.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l3_Click(object sender, EventArgs e)
+        {
+            if (l3.Text == "*")
+            {
+                l3.Text = whonow.Text;
+                l3.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l4_Click(object sender, EventArgs e)
+        {
+            if (l4.Text == "*")
+            {
+                l4.Text = whonow.Text;
+                l4.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l5_Click(object sender, EventArgs e)
+        {
+            if (l5.Text == "*")
+            {
+                l5.Text = whonow.Text;
+                l5.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+            private void l6_Click(object sender, EventArgs e)
+        {
+            if (l6.Text == "*")
+            {
+                l6.Text = whonow.Text;
+                l6.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+
+        private void l7_Click(object sender, EventArgs e)
+        {
+            if (l7.Text == "*")
+            {
+                l7.Text = whonow.Text;
+                l7.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l8_Click(object sender, EventArgs e)
+        {
+            if (l8.Text == "*")
+            {
+                l8.Text = whonow.Text;
+                l8.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+        private void l9_Click(object sender, EventArgs e)
+        {
+            if (l9.Text == "*")
+            {
+                l9.Text = whonow.Text;
+                l9.ForeColor = Color.Red;
+                whonowChange();
+            }
+        }
+    }
+}
